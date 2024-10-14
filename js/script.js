332a8652a5eefd24fd6b5f639256b7f87ade2df4
Gunnar Farup <113795596+Fargun75@users.noreply.github.com>
2024-10-14 20:40:29 +0200

    Add a function to play one round

--- js/script.js
+++ js/script.js
@@ -15,3 +15,34 @@ function getHumanChoice() {
   
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound (humanChoice, computerChoice) {
+ if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("You loose - paper beats rock!");
+    computerScore += 1;
+ } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win - rock beats scissors!");
+    humanScore += 1;
+ } else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win - paper beats rock!");
+    humanScore += 1;
+ } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("You loose - scissors beats paper!");
+    computerScore += 1;
+ } else if (humanChoice === "scissors" && computerChoice === "rock") {
+    console.log("You loose - rock beats scissors!");
+    computerScore += 1;
+ } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win - scissors beats paper!");
+    humanScore += 1;
+ } else {
+    console.log("It's a draw!");
+ }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
