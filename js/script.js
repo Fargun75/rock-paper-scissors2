8f5ef114edbad58584370a19bc6602c0c653f113
Gunnar Farup <113795596+Fargun75@users.noreply.github.com>
2024-10-15 12:44:11 +0200

    Run functions as callback in 'playRound' and add a output if typo

--- js/script.js
+++ js/script.js
@@ -12,37 +12,43 @@ function getHumanChoice() {
     let choice = prompt("Rock, Paper or Scissors?");
     choice = choice.toLowerCase();
     return choice;
-  
+
 }
+
+
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound (humanChoice, computerChoice) {
- if (humanChoice === "rock" && computerChoice === "paper") {
-    console.log("You loose - paper beats rock!");
-    computerScore += 1;
- } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    console.log("You win - rock beats scissors!");
-    humanScore += 1;
- } else if (humanChoice === "paper" && computerChoice === "rock") {
-    console.log("You win - paper beats rock!");
-    humanScore += 1;
- } else if (humanChoice === "paper" && computerChoice === "scissors") {
-    console.log("You loose - scissors beats paper!");
-    computerScore += 1;
- } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    console.log("You loose - rock beats scissors!");
-    computerScore += 1;
- } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    console.log("You win - scissors beats paper!");
-    humanScore += 1;
- } else {
-    console.log("It's a draw!");
- }
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You loose - paper beats rock!");
+        computerScore += 1;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("You win - rock beats scissors!");
+        humanScore += 1;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("You win - paper beats rock!");
+        humanScore += 1;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You loose - scissors beats paper!");
+        computerScore += 1;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You loose - rock beats scissors!");
+        computerScore += 1;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("You win - scissors beats paper!");
+        humanScore += 1;
+    } else if (humanChoice === computerChoice) {
+        console.log("It's a draw!");
+    } else {
+        console.log("I think there is a typo..?");
+    }
+
+    console.log("Your score is " + humanScore);
+    console.log("The computer score is " + computerScore);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
+playRound(getHumanChoice(), getComputerChoice());
+
